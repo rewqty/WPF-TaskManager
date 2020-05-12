2f81624206531bc9897a69fefd9495828b8bc968
rewqty <boss.dima0000@gmail.com>
2020-05-13 04:41:44 +0500

    Auto save/load tasks/completed tasks to json file

---
+++ WPF-TaskManager/Models/TaskModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_TaskManager.Models
+{
+	class TaskModel : INotifyPropertyChanged
+	{
+		private bool _isDone;
+		private string _text;
+
+		public bool IsDone
+		{
+			get { return _isDone; }
+			set
+			{
+				if (_isDone == value)
+					return;
+				_isDone = value;
+				OnPropertyChanged("IsDone");
+			}
+		}
+		public string Text
+		{
+			get { return _text; }
+			set
+			{
+				if (_text == value)
+					return;
+				_text = value;
+				OnPropertyChanged("Text");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ WPF-TaskManager/Services/FileIOService.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_TaskManager.Models;
+
+namespace WPF_TaskManager.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<TaskModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TaskModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TaskModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
